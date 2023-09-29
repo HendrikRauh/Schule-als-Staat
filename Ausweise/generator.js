5c4f143854301486993bbb6d2853bbf0da5140b9
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2023-09-29 16:02:41 +0200

    Update generator.js

--- Ausweise/generator.js
+++ Ausweise/generator.js
@@ -1,12 +1,26 @@
+const sqlite3 = require('better-sqlite3');
 const crypto = require('crypto');
 
-function hashMD5(input) {
-    return crypto.createHash('md5').update(input).digest('hex');
+class Database {
+    constructor(databaseName) {
+        this.db = new sqlite3(databaseName);
+    }
+
+    createPeopleTable() {
+        this.db.prepare('CREATE TABLE IF NOT EXISTS people (id TEXT PRIMARY KEY, firstName, lastName, colorCode INTEGER)').run();
+    }
+
+    generateMD5Hash(input) {
+        return crypto.createHash('md5').update(input).digest('hex');
+    }
+
+    insertPerson(firstName, lastName, colorCode) {
+        const id = this.generateMD5Hash(firstName + lastName);
+        this.db.prepare(`INSERT INTO people (id, firstName, lastName, colorCode) VALUES ('', '', '', )`).run();
+    }
 }
 
-function test() {
-    const input = 'HendrikRauh';
-    const output = hashMD5(input);
-    console.log(output);
-    // 5d5f984317bc1ab7be36442206faa716
-}
+// Usage
+const db = new Database('my-database.db');
+db.createPeopleTable();
+db.insertPerson('Hendrik', 'Rauh', 1);
