7554d183f5d677e7437b2a1a642cf1313ef93fdd
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2023-09-29 16:27:00 +0200

    added the class to the table

--- Ausweise/generator.js
+++ Ausweise/generator.js
@@ -7,21 +7,21 @@ class Database {
     }
 
     createPeopleTable() {
-        this.db.prepare('CREATE TABLE IF NOT EXISTS people (id TEXT PRIMARY KEY, firstName, lastName, colorCode INTEGER)').run();
+        this.db.prepare('CREATE TABLE IF NOT EXISTS people (id TEXT PRIMARY KEY, firstName TEXT, lastName TEXT, colorCode INTEGER, className TEXT)').run();
     }
 
     generateMD5Hash(input) {
         return crypto.createHash('md5').update(input).digest('hex');
     }
 
-    insertPerson(firstName, lastName, colorCode) {
+    insertPerson(firstName, lastName, colorCode, className) {
         const id = this.generateMD5Hash(firstName + lastName);
-        this.db.prepare(`INSERT OR REPLACE INTO people (id, firstName, lastName, colorCode) VALUES ('${id}', '${firstName}', '${lastName}', ${colorCode})`).run();
+        this.db.prepare(`INSERT OR REPLACE INTO people (id, firstName, lastName, colorCode, className) VALUES ('${id}', '${firstName}', '${lastName}', ${colorCode}, '${className}')`).run();
     }
 }
 
 // Usage
 const db = new Database('DATA.db');
 db.createPeopleTable();
-db.insertPerson('Hendrik', 'Rauh', 1);
-db.insertPerson('Raffael', 'Wolf', 0);
+db.insertPerson('Hendrik', 'Rauh', 1, 'J1');
+db.insertPerson('Raffael', 'Wolf', 0, 'J1');
