f5aee890bdcf616aab991dd74a714ed773e48224
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2023-09-29 16:35:49 +0200

    change the column order

--- Ausweise/generator.js
+++ Ausweise/generator.js
@@ -7,21 +7,21 @@ class Database {
     }
 
     createPeopleTable() {
-        this.db.prepare('CREATE TABLE IF NOT EXISTS people (id TEXT PRIMARY KEY, firstName TEXT, lastName TEXT, colorCode INTEGER, className TEXT)').run();
+        this.db.prepare('CREATE TABLE IF NOT EXISTS people (id TEXT PRIMARY KEY, firstName TEXT, lastName TEXT, className TEXT, colorCode INTEGER)').run();
     }
 
     generateMD5Hash(input) {
         return crypto.createHash('md5').update(input).digest('hex');
     }
 
-    insertPerson(firstName, lastName, colorCode, className) {
+    insertPerson(firstName, lastName, className, colorCode) {
         const id = this.generateMD5Hash(firstName + lastName);
-        this.db.prepare(`INSERT OR REPLACE INTO people (id, firstName, lastName, colorCode, className) VALUES ('${id}', '${firstName}', '${lastName}', ${colorCode}, '${className}')`).run();
+        this.db.prepare(`INSERT OR REPLACE INTO people (id, firstName, lastName, className, colorCode) VALUES ('${id}', '${firstName}', '${lastName}', '${className}', ${colorCode})`).run();
     }
 }
 
 // Usage
 const db = new Database('DATA.db');
 db.createPeopleTable();
-db.insertPerson('Hendrik', 'Rauh', 1, 'J1');
-db.insertPerson('Raffael', 'Wolf', 0, 'J1');
+db.insertPerson('Hendrik', 'Rauh', 'J1', 1);
+db.insertPerson('Raffael', 'Wolf', 'J1', 0);
