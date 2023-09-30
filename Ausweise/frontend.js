f22d5c4f479537959a967f703c6d0a3b2b3789f1
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2023-09-30 18:38:34 +0200

    Create frontend.js

---
+++ Ausweise/frontend.js
@@ -0,0 +1,6 @@
+var information = ["Hendrik", "Rauh", "J1", 1];
+
+window.onload = function () {
+  document.getElementById("firstName").innerHTML = information[0];
+  document.getElementById("lastName").innerHTML = information[1];
+};
