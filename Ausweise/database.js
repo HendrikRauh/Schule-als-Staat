7b8cdce0e765a037e7fac3934e5569188a190547
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2023-09-30 23:23:46 +0200

    added another testname

--- Ausweise/database.js
+++ Ausweise/database.js
@@ -43,5 +43,6 @@ const db = new Database("DATA.db");
 db.createPeopleTable();
 db.insertPerson("Hendrik", "Rauh", "J1", "red");
 db.insertPerson("Raffael", "Wolf", "J1", "transparent");
+db.insertPerson("Sarah", "Krautter", "J2", "blue");
 
 module.exports = Database;
