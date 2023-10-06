ce5bd7e165d8096ff29f19a03c14f6a910bf5e8f
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2023-10-06 15:04:39 +0200

    removed the hardcoded salt and moved it to salt.key

--- idCards/validation.js
+++ idCards/validation.js
@@ -1,15 +1,18 @@
 /*
 * This file contains a function that validates an ID card string.
 * The ID card string is a comma-separated string of the first name, surname, class, and an MD5 hash.
-* The MD5 hash is generated from the first name, surname, and class, each separated by "-MBG-".
+* The MD5 hash is generated from the first name, surname, and class, each separated by a salt.
 */
 
+const fs = require('fs');
 const generateMD5Hash = require('./hashing');
+
+const salt = fs.readFileSync('salt.key', 'utf8').trim();
 
 function isValidIdCard(idCardString) {
   const [firstName, surname, className, hash] = idCardString.split(',');
 
-  const expectedHash = generateMD5Hash(`${firstName}-MBG-${surname}-MBG-${className}`);
+  const expectedHash = generateMD5Hash(`${firstName}${salt}${surname}${salt}${className}`);
 
   return expectedHash === hash;
 }
