ec3f99c8d60aed59f74d0ff5952b711b52bb41e9
RaffaelW <rfflwlf@gmail.com>
2023-10-07 12:36:11 +0200

    added a class that build html for the id cards

---
+++ id-cards/html-builder.js
@@ -0,0 +1,73 @@
+/**
+ * This file defines a HtmlBuilder class that can be used to build the html for the website with the id cards
+ * 
+ * usage:
+ * const html = HtmlBuilder.html(
+ *   head(["style.css"]),
+ *   body(
+ *     idCardsContainer(allIdCards(persons, qrCodes))
+ *   )
+ * );
+ */
+class HtmlBuilder {
+  static html(head, body) {
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+        ${head}
+        ${body}
+      </html>`;
+  }
+
+  static head(title, styleSheets) {
+    let head = `
+      <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>${title}</title>`;
+
+    for (const styleSheet of styleSheets) {
+      head += `<link rel="stylesheet" href="${styleSheet}"/>`;
+    }
+    return (head += `</head>`);
+  }
+
+  static body(content) {
+    return `
+      <body>
+      ${content}
+      </body>`;
+  }
+
+  static idCardsContainer(idCards) {
+    return `
+    <div id="container">
+      ${content}
+    </div>`;
+  }
+
+  static allIdCards(persons, qrCodes) {
+    let idCards = "";
+    persons.forEach((person, index) => {
+      idCards += idCard(person, qrCodes[index]);
+    });
+    return idCards;
+  }
+
+  static idCard(person, qrCode) {
+    return `
+      <div id="idCard" style="--border-color: ${person.colorCode};">
+        <div id="title">Schule als Staat</div>
+        <div id="content">
+          <div id="leftSection">
+            <img id="logo" src="mbg-logo-building.svg" alt="Logo of the MBG"/>
+            <div id="firstName">${person.firstName}</div>
+            <div id="lastName">${person.lastName}</div>
+          </div>
+          <div id="qrCode">${qrCode}</div>
+        </div>
+      </div>`;
+  }
+}
+
+module.exports = HtmlBuilder;
