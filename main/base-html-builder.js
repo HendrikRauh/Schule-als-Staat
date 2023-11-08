f8f65879c4b32486037032cb8d82adea903e01b5
Raffael Wolf <146560011+RaffaelW@users.noreply.github.com>
2023-11-08 17:02:12 +0100

    added favicon
    Signed-off-by: Raffael Wolf <146560011+RaffaelW@users.noreply.github.com>

--- main/base-html-builder.js
+++ main/base-html-builder.js
@@ -24,7 +24,9 @@ class BaseHtmlBuilder {
             <head>
                 <meta charset="UTF-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                <title>${title}</title>`;
+                <title>${title}</title>
+                <link rel="icon" type="image/x-icon" href="../resource/sas-logo.ico">
+                `;
 
         for (const styleSheet of styleSheets) {
             head += `<link rel="stylesheet" href="${styleSheet}"/>`;
