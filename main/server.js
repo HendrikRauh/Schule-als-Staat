8fed8b608b3b145059f330933b71d7df682307c4
RaffaelW <rfflwlf@gmail.com>
2023-11-09 19:19:09 +0100

    moved code for checking url attacks into seperate function

--- main/server.js
+++ main/server.js
@@ -9,30 +9,14 @@ const allowedPaths = ["", "id-cards\\"];
 // Creating server
 const server = http.createServer(async (req, res) => {
 
-    // ---------------------------------
-    // prevent path traversal
-    // ---------------------------------
+    let result = checkUrl(req.url);
+    if (!result.isUrlSafe) {
+        res.writeHead(403, { "Content-Type": "text/plain" });
+        res.end("Access denied");
+        return;
+    } 
 
-    // prevent null byte attack
-    if (req.url.indexOf("/0") !== -1) {
-        res.writeHead(403, { "Content-Type": "text/plain" });
-        res.end("Access denied");
-        return;
-    }
-    // prevent access of parent folder
-    if (!/^[a-z0-9-/]+[\.]?[a-z0-9]*$/.test(req.url)) {
-        res.writeHead(403, { "Content-Type": "text/plain" });
-        res.end("Access denied");
-        return;
-    }
-    const safeUrl = path.normalize(req.url).replace(/^(\.\.(\/|\\|$))+/, "").substring(1);
-    const pathString = path.join(__dirname, safeUrl);
-    if (pathString.indexOf(__dirname) !== 0) {
-        res.writeHead(403, { "Content-Type": "text/plain" });
-        res.end("Access denied");
-        return;
-    }
-
+    const safeUrl = result.safeUrl;
     console.log(`safeUrl: '${safeUrl}'`);
 
     // ---------------------------------
@@ -54,7 +38,7 @@ const server = http.createServer(async (req, res) => {
                         contentType = "text/css";
                     } else if (isSvg) {
                         contentType = "image/svg+xml";
-                    } else if(isIco) {
+                    } else if (isIco) {
                         contentType = "image/x-icon";
                     }
                     res.writeHead(200, { "Content-Type": contentType });
@@ -86,3 +70,32 @@ const server = http.createServer(async (req, res) => {
 server.listen(3000, () => {
     console.log("Server on port 3000!");
 });
+
+function checkUrl(url) {
+    // prevent path traversal
+
+    let resultNotSafe = {
+        isUrlSafe: false,
+        safeUrl: null
+    }
+
+    // prevent null byte attack
+    if (url.indexOf("/0") !== -1) {
+        return resultNotSafe;
+    }
+    // prevent access of parent folder
+    if (!/^[a-z0-9-/]+[\.]?[a-z0-9]*$/.test(url)) {
+        return resultNotSafe;
+    }
+
+    const safeUrl = path.normalize(url).replace(/^(\.\.(\/|\\|$))+/, "");
+    const pathString = path.join(__dirname, safeUrl);
+    if (pathString.indexOf(__dirname) !== 0) {
+        return resultNotSafe;
+    }
+
+    return {
+        isUrlSafe: true,
+        safeUrl: safeUrl.substring(1) // remove leading slash
+    };
+}
