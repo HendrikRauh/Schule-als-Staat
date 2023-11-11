b12d777690943e79fb027fd46b7a1d1f6a8deb68
RaffaelW <rfflwlf@gmail.com>
2023-11-11 19:35:58 +0100

    added code to return the duration the person was attendant

--- main/attendance/change-attendance.js
+++ main/attendance/change-attendance.js
@@ -6,15 +6,16 @@ const attendanceTable = Database("DATA.db").Attendance;
  * If the person is currently checked in, the person will be checked out else the person will be checked in.
  * @param {string} id - the id of the person
  * @param {number} timestamp - the time stamp that should be used for the check-in/check-out time in the database
- * @returns if the person gets check out, the time that the person was attendant else `0`
+ * @returns if the person gets check out, the time that the person was attendant else `-1`
  */
 function changeAttendance(id, timestamp) {
     if (attendanceTable.isPersonCheckedIn(id)) {
         attendanceTable.checkOutPerson(id, timestamp);
+        return attendanceTable.getLastAttendanceDuration(id);
     } else {
         attendanceTable.checkInPerson(id, timestamp);
+        return -1;
     }
-    return; // todo
 }
 
 module.exports = changeAttendance;
