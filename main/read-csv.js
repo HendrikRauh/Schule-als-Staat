16cee1642767ed675357898f43f9651987f624f2
RaffaelW <rfflwlf@gmail.com>
2023-11-11 21:35:31 +0100

    added JsDoc

--- main/read-csv.js
+++ main/read-csv.js
@@ -10,13 +10,19 @@
 const fs = require("fs");
 const csv = require("fast-csv");
 
+
+/**
+ * This function reads the content of a CSV file and parses it to a list of people 
+ * @param {string} filePath - The path to your CSV file
+ * @param {readCsvFinishedCallback} onFinished - A callback that is invoked when the content of the file is read and processed completely
+ */
 function readCsvFile(filePath, onFinished) {
     const people = [];
     let currentClass = "";
 
     fs.createReadStream(filePath)
         .pipe(csv.parse({ headers: false, delimiter: ";", trim: true }))
-        .on("error", (error) => console.warn("Error occured while parsing csv: " + error))
+        .on("error", (error) => console.warn("Error occurred while parsing csv: " + error))
         .on("data", (data) => {
             // if the first column contains only digits, the row contains a student
             if (/^\d+$/.test(data[0])) {
@@ -37,5 +43,11 @@ function readCsvFile(filePath, onFinished) {
             onFinished(people);
         });
 }
+
+/**
+ * This callback is invoked when the content of the file is read and processed completely
+ * @callback readCsvFinishedCallback
+ * @param {Array<{firstName: string, lastName: string, className: string}>} people - An array of the people that are read from the csv file
+ */
 
 module.exports = readCsvFile;
