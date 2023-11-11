e5c6834b0d745454af1e0713c117e3ee623ccb2a
RaffaelW <rfflwlf@gmail.com>
2023-11-11 19:13:27 +0100

    added code to change the attendance

---
+++ main/attendance/change-attendance.js
@@ -0,0 +1,20 @@
+const Database = require("../database");
+const attendanceTable = Database("DATA.db").Attendance;
+
+/**
+ * Changes the attendance status of a person based on the current attendance of the person.
+ * If the person is currently checked in, the person will be checked out else the person will be checked in.
+ * @param {string} id - the id of the person
+ * @param {number} timestamp - the time stamp that should be used for the check-in/check-out time in the database
+ * @returns if the person gets check out, the time that the person was attendant else `0`
+ */
+function changeAttendance(id, timestamp) {
+    if (attendanceTable.isPersonCheckedIn(id)) {
+        attendanceTable.checkOutPerson(id, timestamp);
+    } else {
+        attendanceTable.checkInPerson(id, timestamp);
+    }
+    return; // todo
+}
+
+module.exports = changeAttendance;
