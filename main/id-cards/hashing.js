811fe25c53ea81349322f25015cb1acda412a82e
RaffaelW <rfflwlf@gmail.com>
2023-11-11 21:44:45 +0100

    added JSDoc

--- main/id-cards/hashing.js
+++ main/id-cards/hashing.js
@@ -1,9 +1,10 @@
-/*
- * This file contains a function to generate an sha256 hash from a string.
- */
-
 const crypto = require("crypto");
 
+/**
+ * This methods generates a sha256 hash from a string and returns it.
+ * @param {string} inputString - The string that should be hashed
+ * @returns the hashed string
+ */
 function generateHash(inputString) {
     const hash = crypto.createHash("sha256");
     hash.update(inputString);
