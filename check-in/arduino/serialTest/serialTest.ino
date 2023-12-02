82a78943d3b7596cb146d8a5b459e569f30dca03
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2023-12-02 15:44:41 +0100

    updated the test code: 3 Leds and baudrate

--- check-in/arduino/serialTest/serialTest.ino
+++ check-in/arduino/serialTest/serialTest.ino
@@ -1,35 +1,64 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+/*
+This Arduino program tests the serial communication with the scanner using a three-color LED system (Red, Green, and Yellow). 
+The program listens for specific commands sent over the serial port and changes the LED color based on the received command. 
+*/
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
+const String READY_STATUS_COLOR = "RY";
+const String CONNECTED_STATUS_COLOR = "GRY";
+const String DISCONNECTING_STATUS_COLOR = "Y";
+const String IN_OUT_STATUS_COLOR = "G";
+const String IN_STATUS_COLOR = "GY";
+const String OUT_STATUS_COLOR = "GR";
+const String ERROR_STATUS_COLOR = "R";
 
-void setup() {
-  Serial.begin(9600);
-  lcd.begin(16, 2);
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Ready");
+const int LED_RED_PIN = 4;
+const int LED_GREEN_PIN = 7;
+const int LED_YELLOW_PIN = 8;
+
+void setColor(const String& color)
+{
+  digitalWrite(LED_RED_PIN, color.indexOf('R') != -1 ? HIGH : LOW);
+  digitalWrite(LED_GREEN_PIN, color.indexOf('G') != -1 ? HIGH : LOW);
+  digitalWrite(LED_YELLOW_PIN, color.indexOf('Y') != -1 ? HIGH : LOW);
+}
+
+void handleCommand(const String& data, const String& command, const String& color)
+{
+  if (data == command)
+  {
+    setColor(color);
+    Serial.println("OK");
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_YELLOW_PIN, OUTPUT);
+
+  setColor(READY_STATUS_COLOR);
   Serial.println("Ready");
 }
 
-void loop() {
-  if (Serial.available()) {
+void loop()
+{
+  if (Serial.available())
+  {
     String data = Serial.readString();
     data.trim();
-    if (data == "error") {
-      lcd.clear();
-      lcd.print("FEHLER");
-      Serial.println("OK");
-    } else if (data == "in") {
-      lcd.clear();
-      lcd.print("EINGECHECKT");
-      Serial.println("OK");
-    } else if (data == "out") {
-      lcd.clear();
-      lcd.print("AUSGECHECKT");
-      Serial.println("OK");
-    } else {
-      Serial.println("ERROR - Unknown command");
+
+    handleCommand(data, "error", ERROR_STATUS_COLOR);
+    handleCommand(data, "in", IN_STATUS_COLOR);
+    handleCommand(data, "out", OUT_STATUS_COLOR);
+    handleCommand(data, "inout", IN_OUT_STATUS_COLOR);
+    handleCommand(data, "closing", DISCONNECTING_STATUS_COLOR);
+    handleCommand(data, "connected", CONNECTED_STATUS_COLOR);
+
+    if (data != "error" && data != "in" && data != "out" && data != "inout" && data != "ready" && data != "closing" && data != "connected")
+    {
+      Serial.println("ERROR");
     }
   }
 }
